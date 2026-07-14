20101d2c21945f35235f27e9ab57c2017b83515d
gilbertoparente <80535077+gilbertoparente@users.noreply.github.com>
2026-07-14 22:42:37 +0100

    Complete authentication with JWT middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
+
 const router = express.Router();
 
-const { register } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
+
+router.post("/login", login);
+
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.get("/profile", authMiddleware, (req, res) => {
+
+    res.json({
+        message: "Acesso autorizado.",
+        user: req.user
+    });
+
+});
 
 module.exports = router;
